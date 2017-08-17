0ca3416d024fc80795445203b2918d4f7770845b
paikmingyeong <qoralsrud48@naver.com>
2017-08-17 18:16:49 +0900

    백민경

--- js_src/food_page_rolling.js
+++ js_src/food_page_rolling.js
@@ -1,6 +1,45 @@
 $(function(){
 
+  // 선언부
+
+  var currentIndex = 0;
+  var nextIndex = 1;
+  var prevIndex = 0;
+
+  function init(){
+    $('.food-image-list-item').eq(0).addClass('center');
+    $('.food-image-list-item').eq(1).addClass('right');
+    $('.food-image-list-item').eq(2).addClass('left');
+  }
 
 
+  function moveLeft() {
+
+    if (nextIndex >= $('.food-image-list-item').length){
+      nextIndex = 0;
+    }
+
+    // eq()에 음수 값을 넣어주면 뒷 순서부터 차례대로 매칭됨
+    $('.food-image-list-item').eq(currentIndex-1).removeClass('left ani').addClass('right');
+    $('.food-image-list-item').eq(currentIndex).removeClass('center ani').addClass('left ani');
+    $('.food-image-list-item').eq(nextIndex).removeClass('right').addClass('center ani');
+
+    currentIndex = nextIndex;
+    nextIndex++;
+  }
+
+  // 실행부
+  init();
+
+  $('.food-image-list').on('click', function(){
+    nextIndex = currentIndex + 1;
+    moveLeft();
+  });
+  //
+  //$('.food-image-circle').on('click', function(){
+  //
+  //  nextIndex = currentIndex - 1;
+  //  moveRight();
+  //});
 
 });
