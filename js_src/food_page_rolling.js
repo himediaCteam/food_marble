7438783e69b404043d35852692d3c66c42bed5c0
paikmingyeong <qoralsrud48@naver.com>
2017-08-24 15:59:54 +0900

    qoralsr

--- js_src/food_page_rolling.js
+++ js_src/food_page_rolling.js
@@ -4,25 +4,25 @@ $(function(){
 
   var currentIndex = 0;
   var nextIndex = 1;
-  var prevIndex = 0;
+
 
   function init(){
-    $('.food-image-list-item').eq(0).addClass('center');
-    $('.food-image-list-item').eq(1).addClass('right');
-    $('.food-image-list-item').eq(2).addClass('left');
+    $('.food-image-list-item.fs1').eq(0).addClass('center');
+    $('.food-image-list-item.fs1').eq(1).addClass('right');
+    $('.food-image-list-item.fs1').eq(2).addClass('left');
   }
 
 
   function moveLeft() {
 
-    if (nextIndex >= $('.food-image-list-item').length){
+    if (nextIndex >= $('.food-image-list-item.fs1').length){
       nextIndex = 0;
     }
 
     // eq()에 음수 값을 넣어주면 뒷 순서부터 차례대로 매칭됨
-    $('.food-image-list-item').eq(currentIndex-1).removeClass('left ani').addClass('right');
-    $('.food-image-list-item').eq(currentIndex).removeClass('center ani').addClass('left ani');
-    $('.food-image-list-item').eq(nextIndex).removeClass('right').addClass('center ani');
+    $('.food-image-list-item.fs1').eq(currentIndex-1).removeClass('left ani').addClass('right');
+    $('.food-image-list-item.fs1').eq(currentIndex).removeClass('center ani').addClass('left ani');
+    $('.food-image-list-item.fs1').eq(nextIndex).removeClass('right').addClass('center ani');
 
     currentIndex = nextIndex;
     nextIndex++;
@@ -31,7 +31,7 @@ $(function(){
   // 실행부
   init();
 
-  $('.food-image-list').on('click', function(){
+  $('.food-image-list.fs1').on('click', function(){
     nextIndex = currentIndex + 1;
     moveLeft();
   });
