6b48221271c105ac28115deb90a3930027fe4405
paikmingyeong <qoralsrud48@naver.com>
2017-08-30 14:04:03 +0900

    이제 html수정, 메인만 남았다

--- js_src/food_page_rolling.js
+++ js_src/food_page_rolling.js
@@ -1,186 +1,92 @@
 $(function(){
 
-  // 선언부
+  var imageWrap = [];
+  var currentIndex = [];
+  var nextIndex = [];
+  var prevIndex = [];
 
-  var currentIndex = 0;
-  var nextIndex = 0;
-  var prevIndex = 0;
+  for(var i=0; i < $('.food-image-list').length; i++ ){
 
-
-  function init(){
-    $('.food-image-list-item.fs1').eq(0).addClass('center');
-    $('.food-image-list-item.fs1').eq(1).addClass('right');
-    $('.food-image-list-item.fs1').eq(2).addClass('left');
-    $('.food-image-list-item.fs2').eq(0).addClass('center');
-    $('.food-image-list-item.fs2').eq(1).addClass('right');
-    $('.food-image-list-item.fs2').eq(2).addClass('left');
-    $('.food-image-list-item.fs3').eq(0).addClass('center');
-    $('.food-image-list-item.fs3').eq(1).addClass('right');
-    $('.food-image-list-item.fs3').eq(2).addClass('left');
-  }
-
-
-  function moveLeft1() {
-
-    if (nextIndex >= $('.food-image-list-item.fs1').length){
-      nextIndex = 0;
-    }
-
-    // eq()에 음수 값을 넣어주면 뒷 순서부터 차례대로 매칭됨
-    $('.food-image-list-item.fs1').eq(currentIndex-1).removeClass('left ani').addClass('right');
-    $('.food-image-list-item.fs1').eq(currentIndex).removeClass('center ani').addClass('left ani');
-    $('.food-image-list-item.fs1').eq(nextIndex).removeClass('right').addClass('center ani');
-
-    currentIndex = nextIndex;
-    nextIndex++;
-  }
-
-  function moveLeft2() {
-
-    if (nextIndex >= $('.food-image-list-item.fs2').length){
-      nextIndex = 0;
-    }
-
-    // eq()에 음수 값을 넣어주면 뒷 순서부터 차례대로 매칭됨
-    $('.food-image-list-item.fs2').eq(currentIndex-1).removeClass('left ani').addClass('right');
-    $('.food-image-list-item.fs2').eq(currentIndex).removeClass('center ani').addClass('left ani');
-    $('.food-image-list-item.fs2').eq(nextIndex).removeClass('right').addClass('center ani');
-
-    currentIndex = nextIndex;
-    nextIndex++;
-  }
-
-  function moveLeft3() {
-
-    if (nextIndex >= $('.food-image-list-item.fs3').length){
-      nextIndex = 0;
-    }
-
-    // eq()에 음수 값을 넣어주면 뒷 순서부터 차례대로 매칭됨
-    $('.food-image-list-item.fs3').eq(currentIndex-1).removeClass('left ani').addClass('right');
-    $('.food-image-list-item.fs3').eq(currentIndex).removeClass('center ani').addClass('left ani');
-    $('.food-image-list-item.fs3').eq(nextIndex).removeClass('right').addClass('center ani');
-
-    currentIndex = nextIndex;
-    nextIndex++;
-  }
-
-
-  function moveRight1(){
-
-    if (nextIndex <= -1){
-      nextIndex=$('.food-image-list-item.fs1').length-1;
-    }
-    if( prevIndex >= $('.food-image-list-item.fs1').length ){
-      prevIndex = 0;
-    }
-
-
-    $('.food-image-list-item.fs1').eq(prevIndex).removeClass('right ani').addClass('left');
-    $('.food-image-list-item.fs1').eq(currentIndex).removeClass('center ani').addClass('right ani');
-    $('.food-image-list-item.fs1').eq(nextIndex).removeClass('left').addClass('center ani');
-
-    currentIndex = nextIndex;
-    prevIndex = currentIndex+1;
-    nextIndex--;
+    imageWrap[i] = $('.food-image-list').eq(i).children().length;
+    currentIndex[i] = 0;
+    nextIndex[i] = 0;
+    prevIndex[i] = 0;
 
   }
 
-  function moveRight2(){
-
-    if (nextIndex <= -1){
-      nextIndex=$('.food-image-list-item.fs2').length-1;
-    }
-    if( prevIndex >= $('.food-image-list-item.fs2').length ){
-      prevIndex = 0;
+  function moveLeft( index, $currentWrap ){
+    if( nextIndex[index] >= imageWrap[index] ){
+      nextIndex[index] = 0;
     }
 
+    $currentWrap.children().eq(currentIndex[index]-1).removeClass('left ani').addClass('right');
+    $currentWrap.children().eq(currentIndex[index]).removeClass('center ani').addClass('left ani');
+    $currentWrap.children().eq(nextIndex[index]).removeClass('right').addClass('center ani');
+    $currentWrap.next().next().children().eq(currentIndex[index]).removeClass('on');
+    $currentWrap.next().next().children().eq(nextIndex[index]).addClass('on');
 
-    $('.food-image-list-item.fs2').eq(prevIndex).removeClass('right ani').addClass('left');
-    $('.food-image-list-item.fs2').eq(currentIndex).removeClass('center ani').addClass('right ani');
-    $('.food-image-list-item.fs2').eq(nextIndex).removeClass('left').addClass('center ani');
+    currentIndex[index] = nextIndex[index];
+    nextIndex[index]++;
+  }
 
-    currentIndex = nextIndex;
-    prevIndex = currentIndex+1;
-    nextIndex--;
+  function moveRight( index2, $currentWrap2 ){
+
+    if(nextIndex[index2] <= -1){
+      nextIndex[index2] = imageWrap[index2] - 1;
+    }
+
+    if( prevIndex[index2] >= imageWrap[index2] ){
+      prevIndex[index2] = 0;
+    }
+
+    $currentWrap2.children().eq(prevIndex[index2]).removeClass('right ani').addClass('left');
+    $currentWrap2.children().eq(currentIndex[index2]).removeClass('center ani').addClass('right ani');
+    $currentWrap2.children().eq(nextIndex[index2]).removeClass('left').addClass('center ani');
+    $currentWrap2.next().next().children().eq(currentIndex[index2]).removeClass('on');
+    $currentWrap2.next().next().children().eq(nextIndex[index2]).addClass('on');
+
+    currentIndex[index2] = nextIndex[index2];
+    prevIndex[index2] = currentIndex[index2] + 1;
+    nextIndex[index2]--;
 
   }
 
-  function moveRight3(){
+  $('.food-image-list-arrow-right').on('click', function(){
 
-    if (nextIndex <= -1){
-      nextIndex=$('.food-image-list-item.fs3').length-1;
-    }
-    if( prevIndex >= $('.food-image-list-item.fs3').length ){
-      prevIndex = 0;
-    }
+    var index = $(this).parent().prev().index('.food-image-list');
+    var $currentWrap = $(this).parent().prev();
 
+    nextIndex[index] = currentIndex[index] + 1;
+    moveLeft( index, $currentWrap );
 
-    $('.food-image-list-item.fs3').eq(prevIndex).removeClass('right ani').addClass('left');
-    $('.food-image-list-item.fs3').eq(currentIndex).removeClass('center ani').addClass('right ani');
-    $('.food-image-list-item.fs3').eq(nextIndex).removeClass('left').addClass('center ani');
+  });
 
-    currentIndex = nextIndex;
-    prevIndex = currentIndex+1;
-    nextIndex--;
+  $('.food-image-list-arrow-left').on('click', function(){
 
-  }
+    var index2 = $(this).parent().prev().index('.food-image-list');
+    var $currentWrap2 = $(this).parent().prev();
 
-  // 푸드 페이지 아이콘 슬라이딩
-  $(function(){
-
-    $('.food-icon').on('click', function(e){
-
-      e.preventDefault();
-
-      var pageIndex = ( $(this).index('.food-icon') % 3 );
-
-      console.log(pageIndex);
-
-      $('.food-icon>a').removeClass('on');
-      $('.food-icon').eq(pageIndex).children('a').addClass('on');
-
-      $('.food-page').removeClass('on');
-      $('.food-page').eq(pageIndex).addClass('on');
-    })
+    nextIndex[index2] = currentIndex[index2] - 1;
+    prevIndex[index2] = currentIndex[index2] + 1;
+    moveRight( index2, $currentWrap2 );
 
   });
 
 
+  $('.food-icon').on('click', function(e){
 
-  // 실행부
-  init();
+    e.preventDefault();
 
-  $('.food-image-list-arrow.s1.right').on('click', function(){
-    nextIndex = currentIndex + 1;
-    moveLeft1();
-  });
-  $('.food-image-list-arrow.s2.right').on('click', function(){
-    nextIndex = currentIndex + 1;
-    moveLeft2();
-  });
-  $('.food-image-list-arrow.s3.right').on('click', function(){
-    nextIndex = currentIndex + 1;
-    moveLeft3();
+    var pageIndex = ( $(this).index('.food-icon') % 3 );
+
+    console.log(pageIndex);
+
+    $('.food-icon>a').removeClass('on');
+    $('.food-icon').eq(pageIndex).children('a').addClass('on');
+
+    $('.food-page').removeClass('on');
+    $('.food-page').eq(pageIndex).addClass('on');
   });
 
-  $('.food-image-list-arrow.s1.left').on('click', function(){
-
-    nextIndex = currentIndex - 1;
-    prevIndex = currentIndex + 1;
-    moveRight1();
-  });
-  $('.food-image-list-arrow2.s2.left').on('click', function(){
-
-    nextIndex = currentIndex - 1;
-    prevIndex = currentIndex + 1;
-    moveRight2();
-  });
-  $('.food-image-list-arrow3.s3.left').on('click', function(){
-
-    nextIndex = currentIndex - 1;
-    prevIndex = currentIndex + 1;
-    moveRight3();
-  });
 
 });
